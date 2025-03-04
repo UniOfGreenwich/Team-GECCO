75fe4432b960e01b593ced8dabd07c55e88d9125
Charlotte Maher <cm3786s@gre.ac.uk>
2025-03-04 10:23:08 +0000

    Set Up Frontend and Backend (#15)
    - sorted duplicates etc.
    
    Co-authored-by: cmaher5 <cmaher5@ford.com>

--- budgeting-buddy-backend/LogInSystem/src/main/java/budgetingbuddybackend/loginsystem/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package budgetingbuddybackend.loginsystem.repository;
-
-import budgetingbuddybackend.loginsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
