6cb1d6cc4bb10d701d7037bbd05f5fbd0c18c834
Fox, Ethan (E.J.) <efox27+ford@ford.com>
2025-03-27 13:11:40 +0000

    configured the user info context provider function around the app component

--- frontend/BudgetingBuddy-frontend/src/App.tsx
+++ frontend/BudgetingBuddy-frontend/src/App.tsx
@@ -1,27 +1,30 @@
-import { UserMoneyInfoProvider } from './store/UserMoneyInfoStore';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WelcomeComponent from './Pages/WelcomePage/welcome';
-import UserInfo from './Pages/GatherUserInforPage/userInformation';
-import AboutUs from './Pages/AboutUsPage/aboutus';
-import Contact from './Pages/ContactPage/contact';
-import Help from './Pages/HelpPage/help';
-import Privacy from './Pages/PrivacyPage/privacy';
-import './App.css';
+import { UserMoneyInfoProvider } from "./store/UserMoneyInfoStore";
+import { UserInfoContextTypeProvider } from "./store/UserInformationStore";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomeComponent from "./Pages/WelcomePage/welcome";
+import UserInfo from "./Pages/GatherUserInforPage/userInformation";
+import AboutUs from "./Pages/AboutUsPage/aboutus";
+import Contact from "./Pages/ContactPage/contact";
+import Help from "./Pages/HelpPage/help";
+import Privacy from "./Pages/PrivacyPage/privacy";
+import "./App.css";
 
 function App() {
   return (
-    <UserMoneyInfoProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<WelcomeComponent />} />
-          <Route path='/get-started' element={<UserInfo />} />
-          <Route path='/about' element={<AboutUs />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/help' element={<Help />} />
-          <Route path='/privacy' element={<Privacy />} />
-        </Routes>
-      </BrowserRouter>
-    </UserMoneyInfoProvider>
+    <UserInfoContextTypeProvider>
+      <UserMoneyInfoProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WelcomeComponent />} />
+            <Route path="/get-started" element={<UserInfo />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/privacy" element={<Privacy />} />
+          </Routes>
+        </BrowserRouter>
+      </UserMoneyInfoProvider>
+    </UserInfoContextTypeProvider>
   );
 }
 
