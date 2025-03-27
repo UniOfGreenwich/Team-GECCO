b009253514d521e7d21a096ba24c87d16a33c3e2
Fox, Ethan (E.J.) <efox27+ford@ford.com>
2025-03-27 13:10:13 +0000

    created context for user info added state values for email, name, password created global state setting functions to update these values created a hook to intialise the context easily

---
+++ frontend/BudgetingBuddy-frontend/src/hooks/UseUserInfo.tsx
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { UserInformationContext } from "../store/UserInformationStore"
+
+const UseUserInfo = () => {
+    const context  = useContext(UserInformationContext)
+
+    if(context === undefined) {
+        throw new Error("user info context must be used within its provider component check that your component is within it")
+    }
+
+    return context
+}
+
+export {UseUserInfo}
---
+++ frontend/BudgetingBuddy-frontend/src/store/UserInformationStore.tsx
@@ -0,0 +1,73 @@
+import { createContext, useCallback, useMemo, useState } from "react";
+
+import { ReactNode } from "react";
+import { UserInfoContextType, UserInfoStateType } from "../types/UserInfoContextTypes";
+
+interface UserInfoProviderType {
+  children?: ReactNode;
+}
+
+const UserInformationContext = createContext<UserInfoContextType | undefined>(
+  undefined
+);
+
+export const UserInfoContextTypeProvider: React.FC<UserInfoProviderType> = ({
+  children,
+}) => {
+  const [userInfo, setUserInfo] = useState<UserInfoStateType>({
+    email: undefined,
+    name: undefined,
+    password: undefined,
+  });
+
+  const setEmail = useCallback(
+    (email: string) => {
+      setUserInfo((prev) => {
+        return {
+          ...prev,
+          email: email,
+        };
+      });
+    },
+    [setUserInfo]
+  );
+
+  const setName = useCallback(
+    (name: string) => {
+      setUserInfo((prev) => {
+        return {
+          ...prev,
+          name: name,
+        };
+      });
+    },
+    [setUserInfo]
+  );
+
+  const setPassword = useCallback((password: string) => {
+    setUserInfo((prev) => {
+      return {
+        ...prev,
+        password: password,
+      };
+    });
+  }, []);
+
+  const ctxValue = useMemo(() => {
+    return {
+      userInfo: userInfo,
+      setEmail: setEmail,
+      setName: setName,
+      setPassword: setPassword,
+    };
+  }, [userInfo]);
+
+  return (
+    <UserInformationContext.Provider value={ctxValue}>
+      {children}
+    </UserInformationContext.Provider>
+  );
+};
+
+
+export {UserInformationContext}
