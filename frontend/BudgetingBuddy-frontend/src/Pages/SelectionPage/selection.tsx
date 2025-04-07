1e3d24e07d2c85318a61d81e3971d8deabd48664
Fox, Ethan (E.J.) <efox27+ford@ford.com>
2025-04-07 18:26:21 +0100

    fixed bug where would not return to right step when using back button

--- frontend/BudgetingBuddy-frontend/src/Pages/SelectionPage/selection.tsx
+++ frontend/BudgetingBuddy-frontend/src/Pages/SelectionPage/selection.tsx
@@ -137,7 +137,7 @@ const Selection: React.FC = () => {
       <div className='selection-footer'>
         <button
           className='back-button'
-          onClick={() => navigate('/get-started')} // Navigate to /get-started
+          onClick={() => navigate(-1)} 
         >
           Back
         </button>
